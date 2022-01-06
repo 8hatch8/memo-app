15716df72f36c6597b112c912b65a11706d8cbf2
8hatch8 <yutaka.pg@gmail.com>
2022-01-06 20:29:24 +0900

    Add api/memos/create

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get root to: "homes#index"
 
   namespace :api, format: 'json' do
-    resources :memos, only: [:index]
+    resources :memos, only: [:index, :create]
   end
 
 end
